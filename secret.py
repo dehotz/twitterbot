fea32bdb146346f6b33d915735b0e6546cf9a2e6
D. Hotz <37424407+dehotz@users.noreply.github.com>
2018-06-07 16:24:46 -0500

    Delete secret.py

--- secret.py
+++
@@ -1,6 +0,0 @@
-#sep key file
-
-consumer_key = 'aPbe3Rw3Pr2QX5dqUOMq7GI9k'
-consumer_secret = 'lW1EC2SvoCvbSGX4tFcARyQUR0q6QQL8FzGZSpi1umFoEmHzkB'
-access_token = '1000854107580719105-yGuyJcatxoR6Ur8mjt9eVS7Dq8STCL'
-access_secret = 'MmNR1ixsHajIQLOOh8fIOgql4qGAdFnfGps3kM3IUC5Rn'
